1e44cb599c381d4b82647a5723fd5d2a2201c132
Ahmad <ahmad.zahedi121288@gmail.com>
2023-10-10 17:22:00 +0330

    use js for create html tags

--- main.js
+++ main.js
@@ -1,11 +1,505 @@
-const categoriesSection = document.querySelector('#categories');
+const categoriesButtons = [
+    {
+        id: 1,
+        text: 'Occupational Therapy Fundamentals',
+        sectionTitle: 'category one',
+        questions: [
+            {
+                id: 1,
+                score: 100,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 2,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 3,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 4,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 5,
+                score: 500,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 6,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'Anatomy and Physiology',
+        sectionTitle: 'category two',
+        questions: [
+            {
+                id: 1,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 2,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 3,
+                score: 400,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 4,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 5,
+                score: 600,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 6,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            }
+        ]
+    },
+    
+    {
+        id: 3,
+        text: 'Assessment and Evaluation',
+        sectionTitle: 'category three',
+        questions: [
+            {
+                id: 1,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 2,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 3,
+                score: 400,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 4,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 5,
+                score: 600,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 6,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: 'Treatment Interventions',
+        sectionTitle: 'category four',
+        questions: [
+            {
+                id: 1,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 2,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 3,
+                score: 400,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 4,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 5,
+                score: 600,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 6,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            }
+        ]
+    },
+    {
+        id: 5,
+        text: 'Pediatric Occupational Therapy',
+        sectionTitle: 'category five',
+        questions: [
+            {
+                id: 1,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 2,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 3,
+                score: 400,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 4,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 5,
+                score: 600,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 6,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            }
+        ]
+    },
+    {
+        id: 6,
+        text: 'Occupational Therapy Ethics and Professionalism',
+        sectionTitle: 'category six',
+        questions: [
+            {
+                id: 1,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 2,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 3,
+                score: 400,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 4,
+                score: 200,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 5,
+                score: 600,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            },
+            {
+                id: 6,
+                score: 300,
+                body: '',
+                options: [
+                    'A',
+                    'B',
+                    'C',
+                    'D'
+                ]
+            }
+        ]
+    }
+];
 
-function goToCategories() {
-    categoriesSection.scrollIntoView({ behavior: 'smooth' });
+const categoriesButtonsSection = document.querySelector('#categories-button-section');
+const categoriesSection = document.querySelector('#categories-section');
+
+function createCategoriesButtonsElements() {
+    categoriesButtons.forEach((item) => {
+        const btnElement = document.createElement("button");
+        btnElement.innerText = item.text;
+        btnElement.classList.add('category-button', 'p-4');
+        btnElement.addEventListener('click', () => goToCategoryByid(item.id));
+        categoriesButtonsSection.appendChild(btnElement);
+        
+        const categoryElement = document.createElement('a');
+        categoryElement.href = `#category-${item.id}`;
+        categoryElement.id = `category-${item.id}`;
+        categoryElement.classList.add('h-100', 'd-flex', 'flex-column', 'justify-content-between');
+
+        const categoryTitleElement = document.createElement('div');
+        categoryTitleElement.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+
+        const categoryTitleText = document.createElement('div');
+        categoryTitleText.classList.add('category__title');
+        categoryTitleText.innerText = `${item.sectionTitle}`;
+
+        const categoryTitleButton = document.createElement('button');
+        categoryTitleButton.classList.add('back-to-categories-button', 'd-flex', 'justify-content-center', 'align-items-center', 'p-2');
+        categoryTitleButton.addEventListener('click', goToCategories);
+        categoryTitleButton.innerText = '⬆';
+
+        const categoryDivider = document.createElement('div');
+        categoryDivider.classList.add('category--divider');
+
+        const categoryQuestionsElement = document.createElement('div');
+        categoryQuestionsElement.classList.add('row', 'row-cols-3', 'flex-grow-1');
+
+        const questionContainer = document.createElement('div');
+        questionContainer.classList.add('question__container', 'd-flex', 'flex-column', 'my-2');
+
+        categoryTitleElement.appendChild(categoryTitleText);
+        categoryTitleElement.appendChild(categoryTitleButton);
+
+        categoryElement.appendChild(categoryTitleElement);
+        categoryElement.appendChild(categoryDivider);
+
+        categoryElement.appendChild(categoryQuestionsElement);
+
+        categoryQuestionsElement.appendChild(questionContainer);
+
+
+        categoriesSection.appendChild(categoryElement);
+    });
 }
 
-function goToCategoryByIndex(sectionIndex) {
-    const targetCategorySection = document.querySelector(`#category-${sectionIndex}`);
+createCategoriesButtonsElements();
+
+function goToCategories() {
+    categoriesButtonsSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function goToCategoryByid(sectionid) {
+    const targetCategorySection = document.querySelector(`#category-${sectionid}`);
 
     targetCategorySection.scrollIntoView({ behavior: 'smooth' });
 }
